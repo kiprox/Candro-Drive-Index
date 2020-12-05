fe4bbdc94710071c9443edd7516991c818cd21ea
kiprox <70131116+kiprox@users.noreply.github.com>
2020-12-05 20:40:06 +0700

    Add Contact Us title

--- js/app.js
+++ js/app.js
@@ -131,7 +131,7 @@ function nav(path) {
 	}
 
 	html += `</div></li><li class="nav-item">
-    <a class="nav-link" href="${UI.contact_link}" target="_blank">Contact</a>
+    <a class="nav-link" href="${UI.contact_link}" target="_blank">Contact Us</a>
   </li>`;
 
 	var search_text = model.is_search_page ? (model.q || '') : '';
