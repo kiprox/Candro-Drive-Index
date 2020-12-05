c34892923f443cdf773d36cdd6c59baac88ad754
kiprox <70131116+kiprox@users.noreply.github.com>
2020-12-05 13:37:38 +0700

    Update Search button

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@ function nav(path) {
 	var search_bar = `
 </ul>
 <form class="form-inline my-2 my-lg-0" method="get" action="/${cur}:search">
-<input class="form-control mr-sm-2" name="q" type="search" placeholder="Search" aria-label="Search" value="${search_text}" required>
+<input class="form-control mr-sm-2" name="q" type="search" placeholder="keyword" aria-label="Search" value="${search_text}" required>
 <button class="btn ${UI.dark_mode ? 'btn-secondary' : 'btn-outline-success'} my-2 my-sm-0" onclick="if($('#search_bar').hasClass('mdui-textfield-expanded') && $('#search_bar_form>input').val()) $('#search_bar_form').submit();" type="submit">Search</button>
 </form>
 </div>
