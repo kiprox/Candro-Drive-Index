9eab8a5d9196a152c08e95abb355ace738565fbf
kiprox <70131116+kiprox@users.noreply.github.com>
2020-12-08 00:20:00 +0700

    Update Maning

--- js/app.js
+++ js/app.js
@@ -205,7 +205,7 @@ function list(path) {
 	var content = `
   <div class="container"><br>
   <div class="card">
-  <h5 class="card-header" id="folderne"><input type="text" id="folderne" class="form-control" placeholder="🔥 Homepage" value="" readonly><script>document.getElementById("folderne").innerHTML='Current Folder: '+decodeURI(this.window.location.href.substring(window.location.href.lastIndexOf('/',window.location.href.lastIndexOf('/')-1))).replace('/','').replace('/','');</script>
+  <h5 class="card-header" id="folderne"><input type="text" id="folderne" class="form-control" placeholder="🔥 Homepage" value="" readonly><script>document.getElementById("folderne").innerHTML='🔥 '+decodeURI(this.window.location.href.substring(window.location.href.lastIndexOf('/',window.location.href.lastIndexOf('/')-1))).replace('/','').replace('/','');</script>
   </h5>
   <div id="list" class="list-group">
   </div>
