2482342a9b477fac3b71cb7cfcee4ec8f8912e83
kiprox <70131116+kiprox@users.noreply.github.com>
2020-12-08 05:50:27 +0700

    Auto Commit #68 app.js

--- js/app.js
+++ js/app.js
@@ -71,12 +71,12 @@ function title(path) {
 	var cur = window.current_drive_order || 0;
 	var drive_name = window.drive_names[cur];
 	path = path.replace(`/${cur}:`, '');
-	$('title').html(document.siteName); // testing $('title').html(document.siteName + ' - ' + path);
+	// testing $('title').html(document.siteName + ' - ' + path);
 	var model = window.MODEL;
 	if (model.is_search_page)
 		$('title').html(`${drive_name} - Search results for ${model.q} `);
 	else
-		$('title').html(`${drive_name} - ${path}`);
+		$('title').html(document.siteName); //$('title').html(`${drive_name} - ${path}`);
 }
 
 // Render the navigation bar
